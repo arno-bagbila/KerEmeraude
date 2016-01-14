88c6d5ef062d3f60ec16e3d774c19e5ef320658c
arno-bagbila <beat4free@gmail.com>
2016-01-14 16:12:55 +0000

    adding webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require("path");
 const HtmlwebpackPlugin = require("html-webpack-plugin");
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const merge = require("webpack-merge");
+const webpack = require("webpack");
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, "app"),
   build: path.join(__dirname, "build")
 };
 
-module.exports = {
+const common = {
   entry: PATHS.app,
   output: {
     path: PATHS.build,
@@ -20,3 +23,29 @@ module.exports = {
     new OpenBrowserPlugin({ url: 'http://localhost:8080' })
   ]
 };
+
+//Default configuration
+if(TARGET === "start" || !TARGET){
+  module.exports = merge(common, {
+    devServer:{
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      //Display only erros to reduce the amount of output
+      stats: "errors-only",
+
+      //Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins:[
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === "build"){
+  module.exports = merge(common, {});
+}
