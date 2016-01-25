9e467117a3282f5fc3c4d69473604887742aa3d9
arno-bagbila <beat4free@gmail.com>
2016-01-25 15:18:44 +0000

    removing the string Arek

--- app/component.js
+++ app/component.js
@@ -1,5 +1,5 @@
 module.exports = function(){
   var element = document.createElement("h1");
-  element.innerHTML = "Bienvenue a Ker Emeraude Arerk !";
+  element.innerHTML = "Bienvenue a Ker Emeraude !";
   return element;
 }
